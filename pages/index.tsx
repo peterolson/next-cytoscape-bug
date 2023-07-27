403f3907a9d240d876fd47409f6f139de1fe003d
Peter Olson <peter.e.c.olson+github@gmail.com>
2023-07-27 16:56:59 -0500

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,26 @@
+import { useEffect, useRef } from "react";
+import cytoscape from 'cytoscape';
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  const graphNode = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (!graphNode.current) return;
+    cytoscape({
+      container: graphNode.current,
+      elements: [
+        { data: { id: "a" } },
+        { data: { id: "b" } },
+        {
+          data: {
+            id: "ab",
+            source: "a",
+            target: "b",
+          },
+        },
+      ]
+    });
+  }, [graphNode]);
+
+  return <div ref={graphNode} style={{ width: 100, height: 100 }} />
 }
